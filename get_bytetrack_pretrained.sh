b0a45f2d0ce8f015ed6c9c7e78ce0f2a3324c716
Johan Edstedt <johan.edstedt@liu.se>
2021-11-09 18:44:20 +0100

    add download script

---
+++ get_bytetrack_pretrained.sh
@@ -0,0 +1,5 @@
+ggID='1XwfUuCBF4IgWBWK2H7oOhQgEj9Mrb3rz'
+ggURL='https://drive.google.com/uc?export=download'  
+filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"  
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
+curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "/tmp/${filename}"
